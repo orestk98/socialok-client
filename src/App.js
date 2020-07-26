aa19d3e0646f20ed5549001d84e8d198414c6a37
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-07-26 18:49:12 +0200

    primo upload parte client con pagine home, login, signup. Visualizzazione dei post e form login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import Navbar  from './components/Navbar.js';
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+
+
+const theme = createMuiTheme({"palette":{"common":{"black":"#000","white":"#fff"},"background":{"paper":"#fff","default":"#fafafa"},"primary":{"light":"#7986cb","main":"rgba(9, 11, 25, 1)","dark":"#303f9f","contrastText":"#fff"},"secondary":{"light":"#ff4081","main":"rgba(48, 115, 46, 1)","dark":"#c51162","contrastText":"#fff"},"error":{"light":"#e57373","main":"#f44336","dark":"#d32f2f","contrastText":"#fff"},"text":{"primary":"rgba(0, 0, 0, 0.87)","secondary":"rgba(0, 0, 0, 0.54)","disabled":"rgba(0, 0, 0, 0.38)","hint":"rgba(0, 0, 0, 0.38)"}}})
 
 function App() {
   return (
+    <MuiThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+    <Navbar/>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={home}/>
+        <Route exact path="/login" component={login}/>
+        <Route exact path="/signup" component={signup}/>
+      </Switch>
+      </div>
+    </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
